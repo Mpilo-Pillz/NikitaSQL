6d4fc6910b80688e444dd0868ed42a9dc9c287fd
mpilopillz <mpilo.dlamini@yahoo.com>
2022-08-06 14:43:15 +0200

    learning count

--- dvdrental/selectdvd.sql
+++ dvdrental/selectdvd.sql
@@ -6,3 +6,6 @@ SELECT first_name, last_name, email FROM customer;
 -- Can work with or without parenthesis eg Distinct(column) or DISTINCT column
 SELECT DISTINCT(rating) FROM film;
 SELECT DISTINCT rating FROM film;
+
+-- We can count the records with COUNT
+SELECT COUNT(rating) FROM film;
