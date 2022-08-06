ee70eab7466d3a35deb5da1f3169b885ecaa9e0d
mpilopillz <mpilo.dlamini@yahoo.com>
2022-08-06 14:38:46 +0200

    learning DISTINCT

--- dvdrental/selectdvd.sql
+++ dvdrental/selectdvd.sql
@@ -1,1 +1,8 @@
+SELECT * FROM film;
+
 SELECT first_name, last_name, email FROM customer;
+
+-- We use Distinct for when the column has duplicate values
+-- Can work with or without parenthesis eg Distinct(column) or DISTINCT column
+SELECT DISTINCT(rating) FROM film;
+SELECT DISTINCT rating FROM film;
