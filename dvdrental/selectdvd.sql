c17878f9999de6158cf7944b1363281aac823687
mpilopillz <mpilo.dlamini@yahoo.com>
2022-08-26 12:13:45 +0200

    learning IN with NOT

--- dvdrental/selectdvd.sql
+++ dvdrental/selectdvd.sql
@@ -95,4 +95,8 @@ WHERE amount IN (0.99,1.98,1.99)
 SELECT * FROM customer
 WHERE first_name IN ('John', 'Jake', 'Julie')
 
+-- IN WITH NOT
+SELECT COUNT(*) FROM payment
+WHERE amount NOT IN (0.99,1.98,1.99)
+
 
